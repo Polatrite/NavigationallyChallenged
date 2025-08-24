95615cca73d3b701b5e1db4490911204a97a0e7a
Daniel Eichler <polatrite@gmail.com>
2025-08-24 16:54:54 -0600

    chasm tiles, jumping

--- src/gameplay/player.gd
+++ src/gameplay/player.gd
@@ -8,6 +8,11 @@ var movement_locked: float = 0
 var last_moving_dir: Vector2
 var room_starting_pos: Vector2
 var bomb_proj_scene = preload("res://src/gameplay/bomb_proj.tscn")
+var shadow_frames = [462, 461]
+var is_jumping = false
+var is_autojumping = false
+var jump_acc: float = 0.0
+var jump_tween: Tween
 
 func _ready() -> void:
 	camera.connect("room_changed", _on_room_changed)
@@ -18,6 +23,12 @@ func _process(delta: float) -> void:
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	input_dir.normalized()
 	anim_acc -= delta
+	jump_acc -= delta
+	if jump_acc <= 0:
+		is_jumping = false
+
+	set_collision_layer_value(2, !is_jumping)
+	print('collmask', collision_mask)
 
 	if anim_acc <= 0:
 		anim_acc = 0.15
@@ -26,15 +37,32 @@ func _process(delta: float) -> void:
 		else:
 			$Sprite2D.frame = anim_frames[0]
 
-	if movement_locked <= 0:
+	if is_autojumping:
+		input_dir = last_moving_dir
+		velocity = 160 * input_dir
+		move_and_slide()
+
+	if movement_locked <= 0 and !is_autojumping:
 		if input_dir.length_squared() > 0:
 			last_moving_dir = input_dir
 		velocity = speed * input_dir
 		move_and_slide()
 		$Sprite2D.flip_h = sign(input_dir.x) == 1
 
-	if Input.is_action_just_pressed("action_primary"):
-		print('boots!')
+	if is_autojumping or Input.is_action_just_pressed("action_primary"):
+		if !is_jumping:
+			print('boots!')
+			is_jumping = true
+			is_autojumping = true
+			jump_acc = 0.3
+			$Shadow.visible = true
+			$Shadow.frame = shadow_frames[0]
+			var tween = get_tree().create_tween()
+			jump_tween = tween
+			tween.tween_property($Sprite2D, "position", Vector2(0,-24), jump_acc/2)
+			tween.chain()
+			tween.tween_property($Sprite2D, "position", Vector2(0, 0), jump_acc/2)
+			tween.play()
 
 	if Input.is_action_just_pressed("action_2"):
 		if Global.unlocks["bomb"] == true:
@@ -46,9 +74,26 @@ func _process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("action_3"):
 		global_position = room_starting_pos
-	
+
 
 
 func _on_room_changed(room_x, room_y):
 	prints('room changed', room_x, ',', room_y)
 	room_starting_pos = global_position
+
+func stop_jumping():
+	if jump_tween:
+		jump_tween.stop()
+	$Sprite2D.position.y = 0
+	is_autojumping = false
+	is_jumping = false
+	jump_acc = 0
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	prints('body', body)
+	if body is TileMapLayer or body is BombTile:
+		stop_jumping()
+
+func fall_in_pit():
+	stop_jumping()
+	global_position = room_starting_pos
